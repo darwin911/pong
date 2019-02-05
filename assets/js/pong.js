1d7e74e560459e51f25c5d796724c618de516cf0
Darwin Smith <darwinpsmith@gmail.com>
2019-02-05 09:03:42 -0500

    WIP: Continuous Movement() - Adjusted x/y coordinates to use offsetTop/Left so it's relative to it's contianer and not the viewport.

--- assets/js/pong.js
+++ assets/js/pong.js
@@ -11,73 +11,49 @@ const turnBallWhite = () => ball.style.backgroundColor = 'white';
 
 const ballPos =   {x: 0, y: 0};
 const paddlePos = {x: 0, y: 0};
-
 const reset = () => {
+  newMove(0, 0)
   moveBall(); turnBallWhite();
 };
-
 const setBallPosition = () => {
-  const ballPosition = ball.getBoundingClientRect();
-  ballPos.x = ballPosition.x;
-  ballPos.y = ballPosition.y;
+  console.log(ballPos.x);
+  ballPos.x = ball.offsetLeft;
+  ballPos.y = ball.offsetTop;
 };
-
 const setPaddlePosition = () => {
   const paddlePosition = paddle.getBoundingClientRect();
   paddlePos.x = paddlePosition.x;
   paddlePos.y = paddlePosition.y;
 };
-
 const createBall = () => {
   ball.classList.add('ball');
-  ball.style.left = `${0}px`;
-  ball.style.top = `${(board.offsetHeight / 2) - 20}px`;
+  newMove(0, (board.offsetHeight / 2) - 20);
 };
-
 const moveBall = () => {
   ball.style.left = `${ballPos.x}px`;
   ball.style.top = `${ballPos.y}px`;
 };
-
 const newMove = (x, y) => {
+  // console.log(ballPos.x);
   ballPos.x += x;
+  // console.log(ballPos.x);
   ballPos.y += y;
   moveBall();
 }
-
 const continuousMovement = (x, y) => {
   if (!checkForCollision()) {
     newMove(x, y);
   }
   if (checkForCollision()) {
+    newMove(x, -y);
     console.log('deflection');
   }
 }
-
-// setInterval( () => {
-//   continuousMovement(100, 100);
-// }, 100);
-
-// const moveTopCenter = () => { moveBall();
-// };
-// const moveCenterRight = () => moveBall(window.innerWidth - 40, window.innerHeight / 2);
-// const moveBottomCenter = () => moveBall(window.innerWidth / 2, window.innerHeight - 40);
-// const moveCenterLeft = () => moveBall(0, window.innerHeight / 2);
-//
-// const bounceBall = () => {
-//   // debugger;
-//   const topC = setTimeout(moveTopCenter, 0);
-//   const centerR = setTimeout(moveCenterRight, 400);
-//   const bottomC = setTimeout(moveBottomCenter, 800);
-//   const centerL = setTimeout(moveCenterLeft, 1200);
-// };
-
 const checkForCollision = () => {
-  const winW = board.offsetWidth;
-  const winH = board.offsetHeight;
-  if (ballPos.x <= 0 || ballPos.y <= 0 || ballPos.x >= (winW - 40) || ballPos.y >= winH - 40) {
+  const w = board.offsetWidth;
+  const h = board.offsetHeight;
+  if (ballPos.x <= 0 || ballPos.y <= 0 || ballPos.x >= (w - 40) || ballPos.y >= h - 40) {
     turnBallRed();
-    console.log('collision');
     return true;
   } else {
     turnBallWhite();
@@ -85,14 +61,14 @@ const checkForCollision = () => {
   }
 };
 
-// const moveBallWithMouse = e => {
-//   console.log(e.currentTarget);
-//   ball.style.left = `${e.target.offsetWidth}px`;
-//   ball.style.top = `${e.target.offsetHeight}px`;
-//   checkForCollision(e, ballPos);
-// }
+// This function is for debugging
+const moveBallWithMouse = e => {
+  ballPos.x = e.offsetX;
+  ballPos.y = e.offsetY;
+  moveBall();
+}
 // board.addEventListener('mousemove', moveBallWithMouse);
-
+// ----------------------------------------------------------
 const movePaddleWithMouse = e => {
   const y = e.clientY;
   if (y > board.offsetTop  && y < board.offsetHeight) {
@@ -113,5 +89,25 @@ startGame();
 setInterval(checkForCollision, 50);
 setInterval(setBallPosition, 100);
 setInterval(setPaddlePosition, 100);
+setInterval(moveBall(), 10);
 
 // const bounceBallLoop = setInterval(bounceBall, 1600);
+
+
+// setInterval( () => {
+  //   continuousMovement(100, 100);
+  // }, 100);
+
+  // const moveTopCenter = () => { moveBall();
+    // };
+    // const moveCenterRight = () => moveBall(window.innerWidth - 40, window.innerHeight / 2);
+    // const moveBottomCenter = () => moveBall(window.innerWidth / 2, window.innerHeight - 40);
+    // const moveCenterLeft = () => moveBall(0, window.innerHeight / 2);
+    //
+    // const bounceBall = () => {
+      //   // debugger;
+      //   const topC = setTimeout(moveTopCenter, 0);
+      //   const centerR = setTimeout(moveCenterRight, 400);
+      //   const bottomC = setTimeout(moveBottomCenter, 800);
+      //   const centerL = setTimeout(moveCenterLeft, 1200);
+      // };
