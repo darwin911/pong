db0500f61875880c7db72b9d6c32a81ef7f74cce
Darwin Smith <darwinpsmith@gmail.com>
2019-02-06 13:05:15 -0500

    refactored CSS. Aded Header back to Game with functioning Start button and Score. Refactored JS, and renamed constants to be more readable

--- assets/js/pong.js
+++ assets/js/pong.js
@@ -1,12 +1,3 @@
-// const moveBallWithMouse = e => {
-  //   bPos.x = (e.offsetX - 20);
-  //   bPos.y = (e.offsetY - 20);
-  //   move();
-  // }
-  // board.addEventListener('mousemove', moveBallWithMouse);
-
-// This function is for debugging
-
 const body = document.body;
 const banner = body.querySelector('#banner');
 const startButton = body.querySelector('#start-button');
@@ -18,79 +9,94 @@ const ball = body.querySelector('#ball');
 let scoreboard = 0;
 let boardW = board.offsetWidth;
 let boardH = board.offsetHeight;
-// ---------------------------------
+//
 const turnBallRed = () => ball.style.backgroundColor = 'red';
 const turnBallWhite = () => ball.style.backgroundColor = 'white';
-const paddlePos = {x: 0, y: 0};
 
 const movePaddleWithMouse = e => {
+  console.log(e.clientY);
   const y = e.clientY;
-  if (y > board.offsetTop  && y < board.offsetHeight) {
+  if (y > board.offsetTop  && y < (board.offsetHeight + 50)) {
     paddle.style.top = `${y - (board.offsetTop) - 50}px`;
   }
 };
 body.addEventListener('mousemove', movePaddleWithMouse);
 
-const bPos = { x: 0, y: 0, dx: 1, dy: 1};
+const ballPos = { x: 0, y: 0, dx: 1, dy: 1};
+const bSize = 40;
+
 const reset = () => {
-  bPos.x = (boardW / 2) - 20;
-  bPos.y = (boardH / 2) - 20;
-  bPos.dx = 1;
-  scoreboard = 0;
-  moveBall(); turnBallWhite();
+  ballPos.x = (boardW / 2) - 20;
+  ballPos.y = (boardH / 2) - 20;
+  ballPos.dx = 1;
+  ballPos.dy = -1;
+  score.innerText = `Score: ${scoreboard = 0}`
+  turnBallWhite();
 };
+
 const createBall = () => {
   ball.classList.add('ball');
-  move();
+  ball.style.display = "block";
 };
+
 const moveBall = () => {
-  ball.style.left = `${bPos.x}px`;
-  ball.style.top = `${bPos.y}px`;
+  ball.style.left = `${ballPos.x}px`;
+  ball.style.top = `${ballPos.y}px`;
 };
+
 const move = (x, y, dx, dy) => {
-  bPos.x += bPos.dx * (5 * 2);
-  bPos.y += bPos.dy * parseInt(2.1 * 2);
+  ballPos.x += ballPos.dx * (5 * 2);
+  ballPos.y += ballPos.dy * (2.2 * 2);
   moveBall();
-}
+};
 
 const checkForCollision = () => {
-  if (bPos.x < 1 || bPos.y < 10 || bPos.x >= (boardW - 40) || bPos.y >= boardH - 40) {
+  if (ballPos.x <= 0 || ballPos.y < 1 || ballPos.x >= (boardW - bSize) || ballPos.y >= boardH - bSize) {
     turnBallRed(); return true;
   } else {
     turnBallWhite(); return false;
   }
 };
 
-const startGame = () => { reset(); createBall(); };
-startGame();
+const startGame = () => {
+  reset();
+  createBall();
+};
 
-const flipY = () => bPos.dy = -bPos.dy;
-const flipX = () => bPos.dx = -bPos.dx;
+startButton.addEventListener('click', startGame);
+
+const flipY = () => ballPos.dy = -ballPos.dy;
+const flipX = () => ballPos.dx = -ballPos.dx;
 
 const lose = () => {
-  alert(`You lost. Your score was: ${scoreboard}`)
-  reset();
+  console.log('lose');
+  alert(`You lost. Your score was: ${scoreboard}`);
+   reset();
 }
 
-const updateScore = () => {
+const addPoint = () => {
   scoreboard += 1;
   score.innerText = `Score: ${scoreboard}`
 }
 
 setInterval( () => {
-  const x = bPos.x; const y = bPos.y;
+  
+  const x = ballPos.x; const y = ballPos.y;
   const pTop = paddle.offsetTop;
   const pBottom = pTop + 100;
-  console.log(x, y);
+  boardH = board.offsetHeight;
+  boardW = board.offsetWidth;
+
   if ( !checkForCollision() ) {
 
   } else if (x > boardW - 40 ) {
     flipX();
   } else if (y > boardH - 40 || y < 0) {
     flipY();
-  } else if (x < 0 && y > (pTop - 50) && y < pBottom) {
-    flipX(); updateScore();
-  } else if (x < 5) {
+  } else if (x < 1 && y >= (pTop - 50) && y <= pBottom) {
+    flipX(); addPoint();
+  } else if (x < 0) {
     lose();
   }
-  move();} , 10);
+  move();
+} , 26);
