9d633b7622a02e7413b505328230ed45f0147762
Darwin Smith <darwinpsmith@gmail.com>
2019-08-09 12:08:44 -0400

    refactor with prettier

--- assets/js/pong.js
+++ assets/js/pong.js
@@ -6,7 +6,7 @@ const score = body.querySelector('#score');
 const board = body.querySelector('#board');
 const paddle = body.querySelector('#paddle');
 const ball = body.querySelector('#ball');
-const ballObj = { x: 0, y: 0, dx: 1, dy: 1, size: 40};
+const ballObj = { x: 0, y: 0, dx: 1, dy: 1, size: 40 };
 let scoreboard = 0;
 let highestScore = 0;
 let hMove = 1;
@@ -15,27 +15,31 @@ let start;
 
 const movePaddle = e => {
   const mouseY = e.clientY;
-  if (mouseY > board.offsetTop  && mouseY < (board.offsetHeight + 100)) {
-    paddle.style.top = `${mouseY - (board.offsetTop) - 50}px`;
+  if (mouseY > board.offsetTop && mouseY < board.offsetHeight + 100) {
+    paddle.style.top = `${mouseY - board.offsetTop - 50}px`;
   } else if (mouseY === undefined) {
     const touchY = e.targetTouches[0].clientY;
     e.preventDefault();
-    if (touchY > board.offsetTop  && touchY < (board.offsetHeight + 100)) {
-      paddle.style.top = `${touchY - (board.offsetTop) - 50}px`;
+    if (touchY > board.offsetTop && touchY < board.offsetHeight + 100) {
+      paddle.style.top = `${touchY - board.offsetTop - 50}px`;
     }
   }
 };
 
 const reset = () => {
   if (scoreboard > highestScore) highestScore = scoreboard;
-  ballObj.x = (window.innerWidth / 2) - ballObj.size;
-  ballObj.y = (window.innerHeight / 2) - ballObj.size;
-  ballObj.dx = 1; ballObj.dy = (Math.random() < 0.5) ? 1 : -1;
-  hMove = 7; vMove = 5;
-  score.innerText = `${scoreboard = 0}`;
+  ballObj.x = window.innerWidth / 2 - ballObj.size;
+  ballObj.y = window.innerHeight / 2 - ballObj.size;
+  ballObj.dx = 1;
+  ballObj.dy = Math.random() < 0.5 ? 1 : -1;
+  hMove = 7;
+  vMove = 5;
+  score.innerText = `${(scoreboard = 0)}`;
 };
 
-const createBall = () => { ball.classList.add('ball'); };
+const createBall = () => {
+  ball.classList.add('ball');
+};
 
 const move = () => {
   ballObj.x += ballObj.dx * hMove;
@@ -49,8 +53,8 @@ const addPoint = () => {
   score.innerText = `${scoreboard}`;
 };
 
-const flipY = () => ballObj.dy = -ballObj.dy;
-const flipX = () => ballObj.dx = -ballObj.dx;
+const flipY = () => (ballObj.dy = -ballObj.dy);
+const flipX = () => (ballObj.dx = -ballObj.dx);
 
 const lose = () => {
   alert(`
@@ -59,14 +63,18 @@ const lose = () => {
     You lost.
     Score: ${scoreboard}
     Highest Score: ${highestScore}`);
-    reset();
+  reset();
   clearInterval(start);
 };
 
-const deflect = () => (Math.random() < 0.5) ? vMove *= 1.020 : hMove *= 1.01;
+const verticalDeflect = 1.5;
+
+const deflect = () =>
+  Math.random() < 0.5 ? (vMove *= 1.025) : (hMove *= 1.01);
 
 const checkForCollision = () => {
-  const x = ballObj.x; const y = ballObj.y;
+  const x = ballObj.x;
+  const y = ballObj.y;
   const pTop = paddle.offsetTop;
   const pBottom = pTop + 100;
 
@@ -76,7 +84,7 @@ const checkForCollision = () => {
     return true;
   } else if (y >= board.offsetHeight - ballObj.size) {
     return true;
-  } else if (x <= 10 && y >= (pTop - ballObj.size) && y <= pBottom) {
+  } else if (x <= 10 && y >= pTop - ballObj.size && y <= pBottom) {
     return true;
   } else {
     return false;
@@ -84,36 +92,44 @@ const checkForCollision = () => {
 };
 
 const play = () => {
-  const x = ballObj.x; const y = ballObj.y;
-  const dx = ballObj.dx; const dy = ballObj.dy;
+  const x = ballObj.x;
+  const y = ballObj.y;
+  const dx = ballObj.dx;
+  const dy = ballObj.dy;
 
-  if (!checkForCollision()) {} else if (dx >= 0 && dy >= 0) {
-      (y <= 1) ? flipY() : flipX();
+  if (!checkForCollision()) {
+  } else if (dx >= 0 && dy >= 0) {
+    y <= 1 ? flipY() : flipX();
   } else if (dx >= 0 && dy <= 0) {
-      (x >= board.offsetWidth - ballObj.size) ? flipX() : flipY();
+    x >= board.offsetWidth - ballObj.size ? flipX() : flipY();
   } else if (dx <= 0 && dy <= 0) {
-      if (y >= board.offsetHeight - ballObj.size) {
-        flipY();
-      } else if (x <= 10) {
-        flipX(); addPoint(); deflect();
-      }
+    if (y >= board.offsetHeight - ballObj.size) {
+      flipY();
+    } else if (x <= 10) {
+      flipX();
+      addPoint();
+      deflect();
+    }
   } else if (dx <= 0 && dy >= 0) {
-      if (y <= 1) {
-        flipY();
-      } else if (x <= 10) {
-        flipX(); addPoint(); deflect();
-      }
+    if (y <= 1) {
+      flipY();
+    } else if (x <= 10) {
+      flipX();
+      addPoint();
+      deflect();
+    }
   }
   move();
-    if (ballObj.x <= -1) lose();
+  if (ballObj.x <= -1) lose();
 };
 
-const startGame = () => {  
-  (navigator.userAgent.indexOf('Android') !== -1) && document.documentElement.requestFullscreen()
+const startGame = () => {
+  navigator.userAgent.indexOf('Android') !== -1 &&
+    document.documentElement.requestFullscreen();
   clearInterval(start);
   createBall();
   reset();
-  start = setInterval( play, 1000/60);
+  start = setInterval(play, 1000 / 60);
 };
 
 body.addEventListener('mousemove', movePaddle);
